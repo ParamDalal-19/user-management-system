577774a4aca10665d2bc1dc49409b6629ce305d5
ParamDalal-19 <dalalparam19@gmail.com>
2023-06-05 13:00:40 +0530

    modified code for dropdown in visualization id

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  database: "finalvizmap",
+  database: "node_app",
   user: "root",
   password: "",
 });
--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,12 @@ var router = express.Router();
 var database = require("../database");
 
 router.get("/", function (request, response, next) {
-  response.render("data", { title: "Visualization Mapping" });
+  const query = 'SHOW FULL TABLES WHERE Table_type = "VIEW"';
+  database.query(query, (err, results) => {
+    if (err) throw err;
+    const viewNames = results.map((row) => row[Object.keys(row)[0]]);
+    response.render("data", { title: "Visualization Mapping", viewNames });
+  });
 });
 
 router.post("/action", function (request, response, next) {
@@ -84,3 +89,96 @@ router.post("/action", function (request, response, next) {
 });
 
 module.exports = router;
+
+// var express = require("express");
+// var router = express.Router();
+// var database = require("../database");
+
+// router.get("/", function (request, response, next) {
+//   // const query = 'SHOW FULL TABLES WHERE Table_type = "VIEW"';
+//   // database.query(query, (err, results) => {
+//   //   if (err) throw err;
+//   //   const viewNames = results.map((row) => row[Object.keys(row)[0]]);
+//   //   res.render("config", { viewNames });
+//   // });
+//   response.render("data", { title: "Visualization Mapping" });
+// });
+
+// router.post("/action", function (request, response, next) {
+//   var action = request.body.action;
+
+//   if (action == "fetch") {
+//     var query = "SELECT * FROM visualization_mapping";
+//     database.query(query, function (error, data) {
+//       response.json({
+//         data: data,
+//       });
+//     });
+//   }
+
+//   if (action == "Add") {
+//     var visualizationId = request.body.visualizationId;
+//     var columns = request.body.columns;
+//     var sequenceNumber = request.body.sequenceNumber;
+//     var isActive = request.body.isActive;
+
+//     var query = `
+// 		INSERT INTO visualization_mapping
+// 		(visualizationId, columns, sequenceNumber, isActive)
+// 		VALUES ("${visualizationId}", "${columns}", "${sequenceNumber}", "${isActive}")
+// 		`;
+
+//     database.query(query, function (error, data) {
+//       response.json({
+//         message: "Data Added",
+//       });
+//     });
+//   }
+
+//   if (action == "fetch_single") {
+//     var id = request.body.id;
+
+//     var query = `SELECT * FROM visualization_mapping WHERE id = "${id}"`;
+
+//     database.query(query, function (error, data) {
+//       response.json(data[0]);
+//     });
+//   }
+
+//   if (action == "Edit") {
+//     var id = request.body.id;
+//     var visualizationId = request.body.visualizationId;
+//     var columns = request.body.columns;
+//     var sequenceNumber = request.body.sequenceNumber;
+//     var isActive = request.body.isActive;
+
+//     var query = `
+// 		UPDATE visualization_mapping
+// 		SET visualizationId = "${visualizationId}",
+// 		columns = "${columns}",
+// 		sequenceNumber = "${sequenceNumber}",
+// 		isActive = "${isActive}"
+// 		WHERE id = "${id}"
+// 		`;
+
+//     database.query(query, function (error, data) {
+//       response.json({
+//         message: "Data Edited",
+//       });
+//     });
+//   }
+
+//   if (action == "delete") {
+//     var id = request.body.id;
+
+//     var query = `DELETE FROM visualization_mapping WHERE id = "${id}"`;
+
+//     database.query(query, function (error, data) {
+//       response.json({
+//         message: "Data Deleted",
+//       });
+//     });
+//   }
+// });
+
+// module.exports = router;
